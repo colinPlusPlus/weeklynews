83dc416fc344cb5e4e222130c05b2e5ef605ee7f
Colin Williams <Colin@MainSt.local>
2014-09-09 23:01:38 -0400

    updated nav class. fixed parent nav issue

--- classes/nav.php
+++ classes/nav.php
@@ -1,7 +1,7 @@
 <?php
 
 class WeeklyNews_Mobile_Nav_Walker extends Walker_Nav_Menu {
-    
+
     public function start_lvl( &$output, $depth = 0, $args = array() ) {
         $indent = str_repeat("\t", $depth);
         $output .= "\n$indent<ul class=\"subnav-menu\">\n";
@@ -16,34 +16,34 @@ class WeeklyNews_Mobile_Nav_Walker extends Walker_Nav_Menu {
     function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
         global $wp_query;
         $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
- 
+
         //$class_names = $value = '';
- 
+
         //$classes = empty( $item->classes ) ? array() : (array) $item->classes;
- 
+
         //$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
         //$class_names = ' class="'. esc_attr( $class_names ) . '"';
- 
-        
+
+
+
         $output .= $indent . '<li class="cat-news">';
- 
         $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
         $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
         $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
         $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
         $description  = ! empty( $item->description ) ? '<span>'.esc_attr( $item->description ).'</span>' : '';
- 
+
         if($depth != 0) {
             $description = $append = $prepend = "";
         }
- 
+
         $item_output = $args->before;
         $item_output .= '<a'. $attributes .'>';
         $item_output .= $args->link_before .apply_filters( 'the_title', $item->title, $item->ID );
         $item_output .= $description.$args->link_after;
         $item_output .= '</a>';
         $item_output .= $args->after;
- 
+
         $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
     }
 }
@@ -69,34 +69,43 @@ class WeeklyNews_Do_Nav_Walker extends Walker_Nav_Menu{
     function start_el(&$output, $item, $depth = 0, $args = array(), $id = 0) {
         global $wp_query;
         $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
- 
-        //$class_names = $value = '';
- 
-        //$classes = empty( $item->classes ) ? array() : (array) $item->classes;
- 
-        //$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-        //$class_names = ' class="'. esc_attr( $class_names ) . '"';
- 
-        $output .= $indent . '<li class="home current"><a href="'. get_bloginfo( 'url' ) .'"><span class="glyphicon glyphicon-home"></span></a></li>';
-        $output .= $indent . '<li class="cat-news">';
- 
+
+        $class_names = $value = '';
+
+        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+        $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
+        $class_names = ' class="'. esc_attr( $class_names ) . '"';
+
+
+
+        if ( $depth == 0 ){
+
+          $output .= '<li class="home current"><a href="index.html"><span class="glyphicon glyphicon-home"></span></a></li>';
+          $output .= $indent . '<li class="cat-news">';
+        }
+        else{
+          $output .= '<li class=" ">';
+        }
+
+
         $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
         $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
         $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
         $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
         $description  = ! empty( $item->description ) ? '<span>'.esc_attr( $item->description ).'</span>' : '';
- 
+
         if($depth != 0) {
             $description = $append = $prepend = "";
         }
- 
+
         $item_output = $args->before;
         $item_output .= '<a'. $attributes .'>';
         $item_output .= $args->link_before .apply_filters( 'the_title', $item->title, $item->ID );
         $item_output .= $description.$args->link_after;
         $item_output .= '</a>';
         $item_output .= $args->after;
- 
+
         $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
     }
 
